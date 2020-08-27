60a2c6c2d201ab32a510dccc28d7eb7cf1825ed6
medoba diomande <medobadiom@hotmail.fr>
2020-08-27 16:02:05 +0000

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name  { get; set; }
+
+
+    }
+}
